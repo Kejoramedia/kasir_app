7d5a8972738e56d4d7d39485b28e05cfaa40647f
hasan <kejoramedia@gmail.com>
2023-07-04 06:31:56 +0700

    Aplikasi kasir

--- Aplikasi Kasir.py
+++ Aplikasi Kasir.py
@@ -2,42 +2,68 @@ def hitung_total_harga(jumlah, harga):
     return jumlah * harga
 
 
+def format_harga(harga):
+    return f"Rp.{harga:,.0f}"
+
+
 def cetak_struk(jumlah, item, harga):
     total_harga = hitung_total_harga(jumlah, harga)
-    return f"{jumlah} {item}\t\tRp.{harga}\t\tRp. {total_harga}"
+    return f"{jumlah} {item}\t\t{format_harga(harga)}\t\t{format_harga(total_harga)}"
 
 
 if __name__ == '__main__':
     belanjaan = []
 
     while True:
-        jumlah = int(input("Jumlah (Ketik 0 transaksi selesai): "))
-        item = str(input("Item: "))
-        harga = int(input("Harga: Rp."))
+        item = input('Item (Ketik "0" untuk menyelesaikan transaksi): ').title()
+
+        if item == "0":
+            break
+
+        while True:
+            try:
+                harga = int(input("Harga: Rp."))
+                break
+            except ValueError:
+                print("Harga harus angka bulat. Silakan coba lagi.")
+
+        while True:
+            try:
+                jumlah = int(input("Jumlah: "))
+                break
+            except ValueError:
+                print("Jumlah harus angka bulat. Silakan coba lagi.")
 
         belanjaan.append({"jumlah": jumlah, "item": item, "harga": harga})
 
-        lanjut = input("Apakah ada item belanjaan lain? (y/n): ")
-        if lanjut.lower() != 'y':
-            break
+    if belanjaan:
+        total_belanja = 0
 
-    total_belanja = 0
+        with open("struk.txt", "w") as file:
+            for belanja in belanjaan:
+                jumlah = belanja['jumlah']
+                item = belanja['item']
+                harga = belanja['harga']
 
-    for belanja in belanjaan:
-        jumlah = belanja['jumlah']
-        item = belanja['item']
-        harga = belanja['harga']
+                total_harga = hitung_total_harga(jumlah, harga)
+                total_belanja += total_harga
 
-        total_harga = hitung_total_harga(jumlah, harga)
-        total_belanja += total_harga
+                struk = cetak_struk(jumlah, item, harga)
+                print(struk, file=file)
 
-        print(cetak_struk(jumlah, item, harga))
+            print("-----------------------------------------------", file=file)
+            print(f"\t\tTotal Belanja\t{format_harga(total_belanja)}", file=file)
 
-    print("-----------------------------------------------")
-    print(f"\t\tTotal Belanja\tRp.{total_belanja}")
+            while True:
+                try:
+                    uang_tunai = int(input("\nUang Tunai: Rp."))
+                    break
+                except ValueError:
+                    print("Uang tunai harus dalam bentuk angka. Silakan coba lagi.")
 
-    uang_tunai = int(input("\nUang Tunai: Rp."))
-    kembali = uang_tunai - total_belanja
+            kembali = uang_tunai - total_belanja
 
-    print(f"\n\t\tUang Tunai\tRp.{uang_tunai}")
-    print(f"\n\t\tKembali\t\tRp.{kembali}")
+            print(f"\n\t\tUang Tunai\t{format_harga(uang_tunai)}", file=file)
+            print(f"\n\t\tKembali\t\t{format_harga(kembali)}", file=file)
+    else:
+        print("Belanjaan kosong. Transaksi dibatalkan.")
